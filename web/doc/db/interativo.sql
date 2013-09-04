50d42963e8ff45aa7602890a736a5e3355c6a53a
thpoiani@gmail.com <thpoiani@gmail.com@094e63fc-2494-60af-4612-397b49e1a6fc>
2013-09-04 23:41:04 +0000

    mod /doc/db/interativo.sql : alteração de schema

--- web/doc/db/interativo.sql
+++ web/doc/db/interativo.sql
@@ -1,99 +1,147 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+CREATE DATABASE  IF NOT EXISTS `interativo` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `interativo`;
+-- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
+--
+-- Host: poiani.cxdsxdlfvih4.us-west-2.rds.amazonaws.com    Database: interativo
+-- ------------------------------------------------------
+-- Server version	5.5.27
 
-DROP SCHEMA IF EXISTS `interativo` ;
-CREATE SCHEMA IF NOT EXISTS `interativo` DEFAULT CHARACTER SET utf8 ;
-USE `interativo` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Table `interativo`.`formulario`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `interativo`.`formulario` ;
+--
+-- Table structure for table `campo`
+--
 
-CREATE  TABLE IF NOT EXISTS `interativo`.`formulario` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `titulo` VARCHAR(45) NOT NULL ,
-  `cabecalho` TEXT NULL ,
-  `rodape` TEXT NULL ,
-  `status` ENUM('a','i') NULL DEFAULT 'i' ,
-  `data` DATETIME NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `campo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `campo` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(45) NOT NULL,
+  `maxlength` int(11) DEFAULT NULL,
+  `regex` varchar(80) DEFAULT NULL,
+  `tipo` enum('s','i','f','d','b') DEFAULT 's',
+  `status` enum('a','i') DEFAULT 'i',
+  `data` datetime NOT NULL,
+  `id_categoria` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_categoria_campo_idx` (`id_categoria`),
+  CONSTRAINT `fk_categoria_campo` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE UNIQUE INDEX `titulo_UNIQUE` ON `interativo`.`formulario` (`titulo` ASC) ;
+--
+-- Table structure for table `categoria`
+--
 
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categoria` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(45) NOT NULL,
+  `descricao` text,
+  `status` enum('a','i') DEFAULT 'i',
+  `data` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `interativo`.`categoria`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `interativo`.`categoria` ;
+--
+-- Table structure for table `formulario`
+--
 
-CREATE  TABLE IF NOT EXISTS `interativo`.`categoria` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `titulo` VARCHAR(45) NOT NULL ,
-  `descricao` TEXT NULL ,
-  `status` ENUM('a','i') NULL DEFAULT 'i' ,
-  `data` DATETIME NOT NULL ,
-  `id_formulario` INT UNSIGNED NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  CONSTRAINT `fk_formulario_categoria`
-    FOREIGN KEY (`id_formulario` )
-    REFERENCES `interativo`.`formulario` (`id` )
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT)
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `formulario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `formulario` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(45) NOT NULL,
+  `cabecalho` text,
+  `rodape` text,
+  `status` enum('a','i') DEFAULT 'i',
+  `aberto` enum('a','i') DEFAULT 'i',
+  `data` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `titulo_UNIQUE` (`titulo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE INDEX `fk_formulario_categoria_idx` ON `interativo`.`categoria` (`id_formulario` ASC) ;
+--
+-- Table structure for table `formulario_categoria`
+--
 
+DROP TABLE IF EXISTS `formulario_categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `formulario_categoria` (
+  `id_formulario` int(10) unsigned NOT NULL,
+  `id_categoria` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id_formulario`,`id_categoria`),
+  KEY `fk_formulario_categoria_idx` (`id_formulario`),
+  KEY `fk_categoria_formulario_idx` (`id_categoria`),
+  CONSTRAINT `fk_formulario_categoria` FOREIGN KEY (`id_formulario`) REFERENCES `formulario` (`id`),
+  CONSTRAINT `fk_categoria_formulario` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `interativo`.`campo`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `interativo`.`campo` ;
+--
+-- Table structure for table `grupo`
+--
 
-CREATE  TABLE IF NOT EXISTS `interativo`.`campo` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `titulo` VARCHAR(45) NOT NULL ,
-  `maxlength` INT NOT NULL DEFAULT 50 ,
-  `status` ENUM('a','i') NULL DEFAULT 'i' ,
-  `data` DATETIME NOT NULL ,
-  `id_categoria` INT UNSIGNED NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  CONSTRAINT `fk_categoria_campo`
-    FOREIGN KEY (`id_categoria` )
-    REFERENCES `interativo`.`categoria` (`id` )
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT)
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `grupo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `grupo` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(45) NOT NULL,
+  `tipo` enum('c','r') DEFAULT 'c',
+  `data` datetime NOT NULL,
+  `id_campo` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_categoria_grupo_idx` (`id_campo`),
+  CONSTRAINT `fk_grupo_categoria` FOREIGN KEY (`id_campo`) REFERENCES `campo` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE INDEX `fk_categoria_campo_idx` ON `interativo`.`campo` (`id_categoria` ASC) ;
+--
+-- Table structure for table `respostas`
+--
 
+DROP TABLE IF EXISTS `respostas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `respostas` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `prontuario` varchar(7) NOT NULL,
+  `resposta` text NOT NULL,
+  `data` datetime NOT NULL,
+  `id_campo` int(10) unsigned NOT NULL,
+  `id_grupo` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_resposta_campo_idx` (`id_campo`),
+  KEY `fk_resposta_grupo_idx` (`id_grupo`),
+  CONSTRAINT `fk_resposta_grupo` FOREIGN KEY (`id_grupo`) REFERENCES `grupo` (`id`),
+  CONSTRAINT `fk_resposta_campo` FOREIGN KEY (`id_campo`) REFERENCES `campo` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- -----------------------------------------------------
--- Table `interativo`.`respostas`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `interativo`.`respostas` ;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-CREATE  TABLE IF NOT EXISTS `interativo`.`respostas` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `prontuario` VARCHAR(7) NOT NULL ,
-  `resposta` TEXT NOT NULL ,
-  `data` DATETIME NOT NULL ,
-  `id_campo` INT UNSIGNED NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  CONSTRAINT `fk_resposta_campo`
-    FOREIGN KEY (`id_campo` )
-    REFERENCES `interativo`.`campo` (`id` )
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT)
-ENGINE = InnoDB;
-
-CREATE INDEX `fk_resposta_campo_idx` ON `interativo`.`respostas` (`id_campo` ASC) ;
-
-USE `interativo` ;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- Dump completed on 2013-09-04 20:38:15
