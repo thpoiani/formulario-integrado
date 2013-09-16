46941f338e8b11572915714060b5d17e4bb5b7f5
thpoiani@gmail.com <thpoiani@gmail.com@094e63fc-2494-60af-4612-397b49e1a6fc>
2013-09-16 22:34:53 +0000

    mod packages

--- trunk/java/src/com/ifsp/integrado/ui/CriarForm.java
+++ trunk/java/src/com/ifsp/integrado/ui/CriarForm.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package forms;
+package com.ifsp.integrado.ui;
 
 import java.awt.event.*;
 import javax.swing.*;
