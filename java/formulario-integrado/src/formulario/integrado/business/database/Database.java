433a596c151300ab1257f5b5c16aa2bf709608e1
thpoiani@gmail.com <thpoiani@gmail.com@094e63fc-2494-60af-4612-397b49e1a6fc>
2013-11-21 02:09:22 +0000

    mod: refactory database

--- java/formulario-integrado/src/formulario/integrado/business/database/Database.java
+++ java/formulario-integrado/src/formulario/integrado/business/database/Database.java
@@ -4,46 +4,49 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Database {
+
     private Database() {}
-    
+
     /**
      * Método responsável por instanciar banco de dados.
+     *
      * @param database "ifsp" ou "interativo"
-     * @return 
+     * @return
      */
     public static synchronized Connection getInstance(String database) {
-		try {
-			switch (database) {
-				case "ifsp":
-					return IFSPDatabase.getInstance();
-					
-				case "interativo":
-					return InterativoDatabase.getInstance();
-					
-				default:
-					throw new RuntimeException("Não foi possível se conectar com o banco de dados.");
-			}
-		} catch (SQLException | RuntimeException e) {
-			throw new RuntimeException("Falha na conexão com o banco de dados.");
-		}
-	}
-	
-	public static void close(String database) {
-		try {
-			switch (database) {
-				case "ifsp":
-					IFSPDatabase.close();
-					break;
-					
-				case "interativo":
-					InterativoDatabase.close();
-					break;
-					
-				default:
-					throw new RuntimeException("Não foi possível fechar a conexão com o banco de dados.");
-			}
-		} catch (Exception e) {
-			throw new RuntimeException("Falha na conexão com o banco de dados.");
-		}
-	}
+        try {
+            switch (database) {
+                case "ifsp":
+                    return IFSPDatabase.getInstance();
+
+                case "interativo":
+                    return InterativoDatabase.getInstance();
+
+                default:
+                    throw new RuntimeException("Não foi possível se conectar com o banco de dados.");
+            }
+        } catch (SQLException | RuntimeException e) {
+            throw new RuntimeException("Falha na conexão com o banco de dados.");
+        }
+    }
+
+    public static void close(String database) {
+        try {
+            switch (database) {
+                case "ifsp":
+                    IFSPDatabase.close();
+                    break;
+
+                case "interativo":
+                    InterativoDatabase.close();
+                    break;
+
+                default:
+                    throw new RuntimeException("Não foi possível fechar a conexão com o banco de dados.");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Falha na conexão com o banco de dados.");
+        }
+    }
+    
 }
--- java/formulario-integrado/src/formulario/integrado/business/database/IFSPDatabase.java
+++ java/formulario-integrado/src/formulario/integrado/business/database/IFSPDatabase.java
@@ -3,36 +3,35 @@ package formulario.integrado.business.database;
 import java.sql.SQLException;
 
 class IFSPDatabase {
-    
+
     private final static String ADAPTER = "mysql";
     private final static String HOST = "poiani.cxdsxdlfvih4.us-west-2.rds.amazonaws.com";
     private final static String PORT = "3306";
     private final static String DATABASE = "prematricula";
     private final static String USERNAME = "prematricula";
     private final static String PASSWORD = "prematricula";
-
+    
     private static java.sql.Connection connection;
 
     private IFSPDatabase() {}
 
     protected static synchronized java.sql.Connection getInstance() throws java.sql.SQLException {
-            if (connection == null) {
-                    String connectionString = "jdbc:" + ADAPTER + "://" + HOST + ":" + PORT + "/" + DATABASE;
-                    connection = java.sql.DriverManager.getConnection(connectionString, USERNAME, PASSWORD);
-            }
-            return connection;
+        if (connection == null) {
+            String connectionString = "jdbc:" + ADAPTER + "://" + HOST + ":" + PORT + "/" + DATABASE;
+            connection = java.sql.DriverManager.getConnection(connectionString, USERNAME, PASSWORD);
+        }
+        return connection;
     }
 
     protected static void close() {
-            if (connection != null) {
-                    try {
-                            connection.close();
-                            connection = null;
-                    } catch (SQLException e) {
-                            // TODO - RETIRAR EM AMBIENTE DE PRODUÇÃO
-                            e.printStackTrace();
-                    }
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException e) {
+                throw new RuntimeException("Falha na conexão com o banco de dados.");
             }
+        }
     }
-
+    
 }
