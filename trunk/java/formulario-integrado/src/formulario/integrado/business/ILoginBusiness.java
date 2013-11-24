4cd8bf1235605773d1e48c238813f1d2b0690ea2
thpoiani@gmail.com <thpoiani@gmail.com@094e63fc-2494-60af-4612-397b49e1a6fc>
2013-11-24 04:50:03 +0000

    mod: autenticacao

--- trunk/java/formulario-integrado/src/formulario/integrado/business/ILoginBusiness.java
+++ trunk/java/formulario-integrado/src/formulario/integrado/business/ILoginBusiness.java
@@ -1,9 +1,10 @@
 package formulario.integrado.business;
 
 import formulario.integrado.model.Login;
+import java.sql.SQLException;
 
 public interface ILoginBusiness {
     public void openConnection();
     public void closeConnection();
-    public boolean isAuthenticated(Login model);
+    public boolean isAuthenticated(Login model) throws SQLException;
 }
--- trunk/java/formulario-integrado/src/formulario/integrado/business/LoginBusiness.java
+++ trunk/java/formulario-integrado/src/formulario/integrado/business/LoginBusiness.java
@@ -3,8 +3,16 @@ package formulario.integrado.business;
 
 import formulario.integrado.business.database.Database;
 import formulario.integrado.model.Login;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LoginBusiness implements ILoginBusiness {
+    
+    private PreparedStatement ps;
+    private ResultSet rs;
+    private String sql;
+    private boolean isAuth;
     
     protected java.sql.Connection connection;
     
@@ -19,12 +27,28 @@ public class LoginBusiness implements ILoginBusiness {
     }
 
     @Override
-    public boolean isAuthenticated(Login model) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public boolean isAuthenticated(Login model) throws SQLException {
+        this.openConnection();
+        
+        this.rs = null;
+        
+        this.sql = "SELECT p.prontuario "
+                 + "FROM pm_pessoas p WHERE p.prontuario = ? AND p.senha = ?";
+        
+        this.ps = connection.prepareStatement(this.sql);
+        this.ps.setString(1, model.getProntuario());
+        this.ps.setString(2, model.getSenha());
+        
+        this.rs = this.ps.executeQuery();
+        this.isAuth = rs.next();
+        
+        this.closeConnection();
+
+        return checkAuthentication();
     }
     
-    private void checkAuthentication(Login model) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    private boolean checkAuthentication() {
+        return this.isAuth;
     }
     
 }
