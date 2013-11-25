6be97c30d134d31ddccaa93c32dec32dd21cf25f
thpoiani@gmail.com <thpoiani@gmail.com@094e63fc-2494-60af-4612-397b49e1a6fc>
2013-11-25 03:27:44 +0000

    MOD: alteração de fluxo de grupos

--- trunk/java/formulario-integrado/src/formulario/integrado/model/Grupo.java
+++ trunk/java/formulario-integrado/src/formulario/integrado/model/Grupo.java
@@ -7,115 +7,68 @@ public class Grupo extends Model {
     private int id;
     private String titulo;
     private Date data;
+    private int ordem;
+    private boolean status;
     
     private int campoId;
     private Campo campo;
-    
-    private int tipoId;
-    private Tipo tipo;
 
-    /**
-     * @return the id
-     */
-    public int getId() {
-        return id;
-    }
-
-    /**
-     * @param id the id to set
-     */
     public void setId(int id) {
         this.id = id;
     }
+    
+    public int getId() {
+        return this.id;
+    }
 
-    /**
-     * @return the titulo
-     */
     public String getTitulo() {
         return titulo;
     }
 
-    /**
-     * @param titulo the titulo to set
-     */
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
-    /**
-     * @return the data
-     */
     public Date getData() {
         return data;
     }
 
-    /**
-     * @param data the data to set
-     */
     public void setData(Date data) {
         this.data = data;
     }
 
-    /**
-     * @return the campoId
-     */
+    public int getOrdem() {
+        return ordem;
+    }
+
+    public void setOrdem(int ordem) {
+        this.ordem = ordem;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
     public int getCampoId() {
         return campoId;
     }
 
-    /**
-     * @param campoId the campoId to set
-     */
     public void setCampoId(int campoId) {
         this.campoId = campoId;
     }
 
-    /**
-     * @return the campo
-     */
     public Campo getCampo() {
         return campo;
     }
 
-    /**
-     * @param campo the campo to set
-     */
     public void setCampo(Campo campo) {
         this.campo = campo;
     }
-
-    /**
-     * @return the tipoId
-     */
-    public int getTipoId() {
-        return tipoId;
-    }
-
-    /**
-     * @param tipoId the tipoId to set
-     */
-    public void setTipoId(int tipoId) {
-        this.tipoId = tipoId;
-    }
-
-    /**
-     * @return the tipo
-     */
-    public String getTipo() {
-        return tipo.getDescricao();
-    }
     
-    public Tipo getTipoModel() {
-        return tipo;
-    }
-
-    /**
-     * @param tipo the tipo to set
-     */
-    public void setTipo(Tipo tipo) {
-        this.tipo = tipo;
-    }
-
     @Override
     public boolean validate() {
         if (this.titulo.isEmpty() || this.titulo == null ) {
